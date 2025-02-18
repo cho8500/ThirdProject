9cd10f1c87cf5a7b1e4bb348a532dd599af33248
ezen <ezen@ezen.com>
2025-02-18 09:55:07 +0900

    0217

--- py-scripts/조/run_all.py
+++ py-scripts/조/run_all.py
@@ -2,20 +2,23 @@ import subprocess
 
 # 실행할 Python 파일
 scripts = [
-    "./조/discussion_list_crawling.py",
-    "./조/discussion_post_crawling.py",
-    "./조/discussion_sentiment.py"
+    "./discussion_list_crawling.py",
+    "./discussion_post_crawling.py",
+    "./discussion_sentiment.py"
 ]
 
 for script in scripts:
+    try :
+        print(f"실행 중: {script}")
 
-    print(f"실행 중: {script}")
+        #result = subprocess.run(["python", script], capture_output=True, text=True)
+        result = subprocess.run(["python", script], stdout=None, stderr=None, text=True)
+        #print(result.stdout)
 
-    result = subprocess.run(["python", script], capture_output=True, text=True)
-    print(result.stdout)
-
-    if result.stderr:
-        print(f"[ERROR] {script}\n{result.stderr}")
-        break
+        if result.stderr:
+            print(f"[ERROR] {script}\n{result.stderr}")
+            break
+    except Exception as e :
+        print(e)
 
 print("[INFO] 모든 스크립트 실행 완료")
